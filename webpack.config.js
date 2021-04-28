f6485a9bc3d7fe598f8a4f2f72eaca4cf71a2fac
J M Rossy <jm.rossy@gmail.com>
2021-04-28 11:46:16 -0400

    Set up basic walletconnect test app

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const webpack = require('webpack');
+const path = require('path');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      openAnalyzer: false,
+    })
+  ],
+  resolve: {
+    fallback: { 
+      "crypto": require.resolve("crypto-browserify"),
+      "stream": require.resolve("stream-browserify")
+    }
+  }
+};
+
+module.exports = config;
